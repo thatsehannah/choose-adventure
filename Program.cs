4ee4aadb5f3d64daf531d6cbf589a20402b3e185
E. Hannah <echannah631@gmail.com>
2023-08-10 16:20:21 -0500

    changes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ChooseYourOwnAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* THE MYSTERIOUS NOISE */
+
+            // Start by asking for the user's name:
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! Welcome to our story.");
+
+
+        }
+    }
+}
+
+
 
